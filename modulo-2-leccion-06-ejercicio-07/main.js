b00ced1f946c57d9cbe6c1548e0fd690b79fafea
GitTher <estherfernandezflorez@gmail.com>
2021-12-24 20:39:24 +0100

    add module 2 lesson 06 exercise 07

---
+++ modulo-2-leccion-06-ejercicio-07/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const userInfo = {
+
+};
+
+const job = 'developer';
+
+userInfo.firstName = 'Francisca';
+userInfo.lastName = 'Francisquez';
+userInfo.age = 33;
+userInfo.job = job;
+
+console.log(userInfo);
+
+userInfo.firstName = 'Paca';
+userInfo.age = userInfo.age + 1;
+
+console.log(userInfo);
+
+/* Vamos a crear un objeto para almacenar la información de un usuario y una constante llamada job donde guardaremos el valor 'developer'. A continuación seguiremos los siguiente pasos:
+
+Usando la notación con punto o la notación con corchetes ([]) (ej: obj.prop o obj["prop"]):
+
+Añadiremos una propiedad llamada firstName y le asignaremos un valor.
+
+Añadiremos una propiedad llamada lastName y le asignaremos un valor.
+
+Añadiremos una propiedad llamada age y le asignaremos un valor numérico.
+
+Añadiremos una propiedad job a la que asignaremos el valor de la constante job.
+
+Comprobaremos que al obtener el valor de cada una de las propiedades que hemos definido hasta ahora, este es correcto
+
+Cambiaremos el nombre del usuario por otro distinto
+
+Aumentaremos en 1 la edad del usuario
+
+Comprobaremos de nuevo que todo sigue funcionando correctamente */
