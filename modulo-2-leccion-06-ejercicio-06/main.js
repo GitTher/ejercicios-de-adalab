af8399c745e59369efcf2177a9a11020705d3c1b
GitTher <estherfernandezflorez@gmail.com>
2021-12-24 20:31:23 +0100

    add module 2 lesson 06 exercise 06

---
+++ modulo-2-leccion-06-ejercicio-06/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const peras = {
+    maxPeras: '',
+    minPeras: '',
+    actuPeras: '',
+    iniPeras: '',
+};
+
+console.log(peras.actuPeras);
+
+peras.actuPeras = 1;
+
+console.log(peras.actuPeras);
+
+peras.actuPeras = -1;
+
+console.log(peras.actuPeras);
+
+
+/* Vamos a crear un objeto que sea una cesta de peras y que debe tener varias propiedades:
+
+Número máximo de peras
+Número mínimo de peras
+Número actual de peras
+Número inicial de peras
+
+Y varios métodos que hagan:
+Añadir al cesto una pera
+Sacar del cesto una pera
+Restablecer el número de peras al valor inicial.
+
+Con la ayuda de console.log probaremos a usar varios métodos distintos y comprobar el número actual de peras para ver si funciona correctamente. */
